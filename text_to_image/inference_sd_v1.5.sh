8cc6bef3b517bfd709b2f68d6ff0a15612e5b415
life <914781743@qq.com>
2024-04-01 10:47:23 +0800

    fix override inference result

--- text_to_image/inference_sd_v1.5.sh
+++ text_to_image/inference_sd_v1.5.sh
@@ -6,4 +6,4 @@ source  ../env.sh
 
 # python text_to_image.py
 #accelerate launch text_to_image.py
-accelerate launch --num_processes=2 text_to_image.py --dist_inference True
+accelerate launch --num_processes=4 text_to_image.py --dist_inference True
